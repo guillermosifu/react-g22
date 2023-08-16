b03952d2f39222436a8758f3becacbef61a8c1a5
guillermosifu <sifuentes1201@gmail.com>
2023-08-15 22:04:23 -0500

    react

---
+++ src/AppEjemplo.jsx
@@ -0,0 +1,20 @@
+// todo lso compoente son a modofunciones
+// todo componente debe iniciar con mayuscula / los archivos tambien
+
+// el pimer paso es deifnir el nombre acorde al componenete
+//recordemos ue el componenet no deja de ser una funncion
+// vamos a conocer Estados //
+// un funcion propia de react UseState =  maneja el estadp de una variable
+// dentro siempre usar cierres en la etiqueta 
+
+const MiPrimerComponente = () => {
+
+  return (
+  <header>
+    <h2>encabezado</h2>
+  </header>
+
+  )
+};
+
+export default MiPrimerComponente;
