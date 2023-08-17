f3f5fc8ecaf03e7b4f9f4ccb3ecf5f8238bf8c4f
guillermosifu <sifuentes1201@gmail.com>
2023-08-16 21:43:17 -0500

    form

--- src/AppEjemplo.jsx
+++ src/AppEjemplo.jsx
@@ -5,16 +5,15 @@
 //recordemos ue el componenet no deja de ser una funncion
 // vamos a conocer Estados //
 // un funcion propia de react UseState =  maneja el estadp de una variable
-// dentro siempre usar cierres en la etiqueta 
+// dentro siempre usar cierres en la etiqueta
 
+import profile from "./assets/logo-coffe.jpeg";
 const MiPrimerComponente = () => {
-
   return (
-  <header>
-    <h2>encabezado</h2>
-  </header>
-
-  )
+    <header>
+      <h2>encabezado</h2>
+    </header>
+  );
 };
 
 export default MiPrimerComponente;
